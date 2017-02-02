a23e524a5e8cc453814c9532810123c0e7ee7b5b
SatabdeeM <mohapatra.satabdee@gmail.com>
2017-02-02 20:04:44 +0530

    angularModule2assignment

--- app.js
+++ app.js
@@ -1,35 +1,48 @@
 (function (){
 'use strict';
 
-angular.module('LunchCheck', [])
-.controller('LunchCheckController', LunchCheckController);	
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListCheckOffService',ShoppingListCheckOffService);	
 
-LunchCheckController.$inject = ['$scope'];
-function LunchCheckController($scope){
-	$scope.list = "";
-	$scope.message="";
-	
-	$scope.displayMessage = function(){
-		var totalItem = calculateTotalItem($scope.list);
-		if(totalItem==0){
-			$scope.message="Please enter data first";
-		}
-		else if(totalItem<=3){
-			$scope.message="Enjoy!";
-		}
-		else{
-			$scope.message="Too much!";
-		}
-	};
-	
-	function calculateTotalItem(string){
-		var count = 0;
-		var words = string.split(","); 
-		for (var i=0 ; i < words.length ; i++){
-		if (words[i] != "")
-          count += 1; 
-    }
-		return count;
-	}
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService) {
+  var itemToBuy = [
+	{itemName : 'cookies', itemQuantity : '10'},
+	{itemName : 'napkins', itemQuantity : 15}
+  ];
+  
+
+  itemToBuy.itemName = "";
+  itemToBuy.itemQuantity = "";
+
+  itemToBuy.addItemToBoughtList = function () {
+    ShoppingListCheckOffService.addItemToBoughtList(itemToBuy.itemName, itemToBuy.itemQuantity);
+  }
+}
+
+
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController(ShoppingListCheckOffService) {
+}
+
+function ShoppingListCheckOffService() {
+  var service = this;
+
+  // List of shopping items
+  var itemAdder = [];
+
+  service.addItemToBoughtList = function (itemName, quantity) {
+    var items = {
+      name: itemName,
+      quantity: quantity
+    };
+    items.push(items);
+  };
+
+    service.getItems = function () {
+    return items;
+  };
 }
 })();
