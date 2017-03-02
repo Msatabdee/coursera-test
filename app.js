06e59024a1d09887558cb275fc9f958d92f8cb7c
SatabdeeM <mohapatra.satabdee@gmail.com>
2017-03-02 18:20:02 +0530

    sd

--- app.js
+++ app.js
@@ -1,75 +1,110 @@
-(function(){
-'use strict';	
+(function () {
+'use strict';
 
-angular.module('NarrowItDownApp',[])
-.controller('NarrowItDownController',NarrowItDownController)
+angular.module('NarrowItDownApp', [])
+.controller('NarrowItDownController', NarrowItDownController)
 .service('MenuSearchService',MenuSearchService)
-.directive('foundItems', foundItemsDirective);
+.directive('foundItems', FoundItems);
 
+function FoundItems() {
+  var ddo = {
+    template: '{{ item.name }}, {{ item.short_name }}, {{ item.description }}',
+    // scope: {
+    //   found: '<' //will show up in the html doc as founds.items
+    // },
+    // controller: FoundItemsDirectiveController,
+    // controllerAs: 'founds',
+    // bindToController: true
+  };
 
-function foundItemsDirective() {
-	var ddo = {
-		templateUrl: 'foundItems.html',
-		scope: {
-		  items: '<',
-		  onRemove: '&'
-		},
-		controller: foundItemsDirectiveController,
-		controllerAs: 'list',
-		bindToController: true
-		
-	  };
   return ddo;
 }
 
-function foundItemsDirectiveController() {
-  var list = this;
-}
+// function FoundItemsDirectiveController() {
+//   var list = this;
+
+//   list.cookiesInList = function () {
+//     for (var i = 0; i < list.items.length; i++) {
+//       var name = list.items[i].name;
+//       if (name.toLowerCase().indexOf("cookie") !== -1) {
+//         return true;
+//       }
+//     }
+
+//     return false;
+//   };
+// }
 
 NarrowItDownController.$inject = ['MenuSearchService'];
-function NarrowItDownController(MenuSearchService){
-	var list = this;
-	list.getMatchedMenuItems = function(){
-		
-		var promise = MenuSearchService.getMatchedMenuItems(list.searchTerm);
+  function NarrowItDownController(MenuSearchService) {
 
-		  promise.then(function (response) {
-			console.log("in ctrl app  "+response.data.menu_items[1].name)
-			list.items = [];
-			list.error="";
-			for (var i = 0; i < response.data.menu_items.length; i++) {
-			  var name = response.data.menu_items[i].name;
-			  if (name.toLowerCase().indexOf(list.searchTerm) !== -1) {
-				 var item = {
-					  name: response.data.menu_items[i].name,
-					  description: response.data.menu_items[i].description
-					};
-				 list.items.push(item);
-			  }
-			}
-			console.log(list.items);
-		  });
-	};
-	list.removeItem = function (itemIndex) {
-		MenuSearchService.removeItem(itemIndex);
-	};
-}	
+  var menu = this;
 
-MenuSearchService.$inject = ['$http'];
-function MenuSearchService($http){
-	var service=this;	
-	
-	service.getMatchedMenuItems = function(searchTerm) {
-		var response = $http({
-		  method: "GET",
-		  url: ("https://davids-restaurant.herokuapp.com/menu_items.json")
-		
-		});
-		return response;
-	};
-	
-	service.removeItem = function (itemIndex) {
-		items.splice(itemIndex, 1);
-	};
+  menu.searchTerm = "";
+
+
+  menu.found = function () {
+    var promise = MenuSearchService.getMatchedMenuItems(menu.searchTerm);
+    promise.then(function(result) {
+        //console.log(result);
+        menu.MatchedItems = result;
+        console.log(menu.MatchedItems);
+        if (menu.MatchedItems.length == 0){
+              menu.message = "Nothing Found";
+              console.log(menu.message);
+            }
+          else{
+              menu.message = "";
+              }
+
+            menu.MatchedItems = result
+            
+    });  
+  };
+
+  menu.removeItem = function (itemIndex) {
+    console.log(menu.MatchedItems[itemIndex]);
+    menu.MatchedItems.splice(itemIndex, 1);
+
+  };
+
+
 }
+
+MenuSearchService.$inject =['$http'];
+function MenuSearchService ($http) {
+  var service = this;
+
+
+  service.getMatchedMenuItems = function (searchTerm) {
+    
+
+  return $http({
+      method: "GET",
+      url: "https://davids-restaurant.herokuapp.com/menu_items.json"
+    }).then(function (response) {
+          var foundItems = [];
+          if(searchTerm.length == 0) {
+            return foundItems;
+          }
+
+
+      // process result and only keep items that match
+      var allItems = response.data.menu_items;
+      var length = allItems.length;
+
+      for (var i = 0; i < length; i++) {
+          var item = allItems[i];
+          if (item.description.indexOf(searchTerm) != -1) {
+            foundItems.push(item);
+          }
+      }
+      // return processed items
+      return foundItems;
+  });
+
+  }; //end of getMatchedItems
+
+}//end of service
+
 })();
